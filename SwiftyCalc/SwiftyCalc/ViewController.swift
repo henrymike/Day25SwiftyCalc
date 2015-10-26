bc5d6b56d746bef3558b99423d6d812ce8d68f70
Mike Henry <chsmike@gmail.com>
2015-10-26 15:14:56 -0400

    Basic addition from equals button

--- SwiftyCalc/SwiftyCalc/ViewController.swift
+++ SwiftyCalc/SwiftyCalc/ViewController.swift
@@ -12,56 +12,64 @@ import Foundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel :UILabel!
-    var currentOperator :NSString!
     
-    var leftNumber = 0.0;
-    var rightNumber = 0.0;
-    var result = 0.0;
+    var currentOperator = ""
+    var leftNumber = 0.0
+    var rightNumber = 0.0
+    var result = 0.0
+    
     
     //MARK: - Interactivity Methods
     
-//    @IBAction func testButtonPressed(sender: UIButton) {
-//        print("Button Pressed")
-//    }
-    
     @IBAction func buttonClearPressed(sender: UIButton) {
         print("Clear")
-        displayLabel.text = ""
-        leftNumber = 0
-        rightNumber = 0
-        result = 0
+        displayLabel.text = "0"
+        leftNumber = 0.0
+        rightNumber = 0.0
+        result = 0.0
         currentOperator = ""
     }
     
     @IBAction func button1Pressed(sender: UIButton) {
         print("1")
-        leftNumber = 1
+//        leftNumber = 1.0
+        displayLabel.text = String(1)
         if currentOperator == "" {
             leftNumber = Double(displayLabel.text!)!
         } else {
             rightNumber = Double(displayLabel.text!)!
         }
-//        currentOperator == "" ? leftNumber = (Float(format: "%f", displayLabel.text!)) : rightNumber = 0
-//        currentOperator == "" ? leftNumber = "displayLabel.text".floatValue : rightNumber = "displayLabel.text".floatValue
         print(leftNumber, rightNumber)
     }
     
     @IBAction func button2Pressed(sender: UIButton) {
         print("2")
-        rightNumber = 2
+//        rightNumber = 2
+        displayLabel.text = String(2)
+        if currentOperator == "" {
+            leftNumber = Double(displayLabel.text!)!
+        } else {
+            rightNumber = Double(displayLabel.text!)!
+        }
     }
     
     @IBAction func buttonPlusPressed(sender: UIButton) {
         print("+")
+        currentOperator = "+"
     }
     
     @IBAction func buttonMinusPressed(sender: UIButton) {
         print("-")
+        currentOperator = "-"
     }
     
     @IBAction func buttonEqualsPressed(sender: UIButton) {
         print("=")
-        result = leftNumber + rightNumber
+        if currentOperator == "+" {
+            result = leftNumber + rightNumber
+            leftNumber = result
+        }
+        print(leftNumber, rightNumber)
         displayLabel.text = (String(format: "%f", result))
     }
     
@@ -71,6 +79,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentOperator = ""
+        displayLabel.text = "0"
 
     }
 
